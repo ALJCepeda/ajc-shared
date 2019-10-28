77362e52bf45764aea49635cf5732f799f1658c9
Alfred Cepeda <alfredjcepeda@gmail.com>
2019-10-28 12:06:30 -0400

    Move convertined to TS and makes Forms easier to work with

--- src/models/TimelineEntry.ts
+++ src/models/TimelineEntry.ts
@@ -1,21 +1,31 @@
 import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {Moment} from "moment";
 
 @Entity('timeline_entry')
 export default class TimelineEntry {
   @PrimaryGeneratedColumn()
-  id:number;
+  id?:number;
 
   @Column()
   message:string;
 
+  @Column()
+  imageURL:string;
+
   @Column({
     nullable: true
   })
-  link:string;
+  labelURL:string;
 
   @Column()
   label:string;
 
   @Column()
-  createdOn:Date;
+  when:Date | Moment;
+
+  @Column()
+  createdOn?:Date;
+
+  @Column()
+  updatedOn?:Date;
 }
